6a40f317a689fe3b6009f8e315591b521004ec9a
mukhulaazam <mukhulaazam@gmail.com>
2025-02-22 23:03:05 +0600

    comment rm

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -11,9 +11,6 @@ export const useAuth = () => {
   const router = useRouter();
   const [user, setUser] = useState<User | null>(null);
 
-  // Query for fetching user data
-  // This query is used to fetch the user data from the server
-  // It is not automatically fetched, but can be fetched manually by calling the refetchUser function
   const { data: userData, refetch: refetchUser } = useQuery({
     queryKey: ['user'],
     queryFn: async () => {
